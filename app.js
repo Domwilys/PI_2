8c525bfc6f464bd2801fbc793eb2080684f889df
Willyan Paproski Bueno <willyan@pop-os.localdomain>
2022-10-14 17:13:25 -0300

    Arquivos sendform.js e app.js comentados

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const app = express();
-const handlebars = require('express-handlebars');
-const bodyParser = require('body-parser');
-const Sugest = require('./models/Sugest');
+const express = require('express');//Insere o "express" no código
+const app = express();//Armazena o "express" em uma variável
+const handlebars = require('express-handlebars');//Insere o "handlebars" no código
+const bodyParser = require('body-parser');//Insere o "bodyParser" no código
+const Sugest = require('./models/Sugest');//Armazena as tabelas do banco de dados criadas no arquivo "Sugest.js"
 
-app.use(express.static('public'));
-app.use(express.static('imagens'));
+app.use(express.static('public'));//Deixa a pasta "public" pública 
+app.use(express.static('imagens'));//Deixa a pasta de imagens pública
 
+//Configurações para o handlebars
 app.engine('handlebars', handlebars.engine({
     defaultLayout: 'main',
     runtimeOptions: {
@@ -20,16 +21,19 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+//Cria a rota da página principal e renderiza o arquivo "index.handlebars"
 app.get("/", function(req, res){
     res.render('index');
 });
 
+//Cria a rota da lista de sugestões, renderiza o arquivo "sugestões.handlebars" e cria um objeto que armazena os valores que o usuário envia para oo banco de dados
 app.get("/sugestoes", function(req, res){
     Sugest.findAll().then(function(posts){
         res.render('sugestoes', {posts: posts});
     });
 });
 
+//Armazena os dados que vão ser cadastrados pelo usuário em objetos, e os envia para o banco de dados através da rota principal. Também foi feita uma verificação para que o preenchimento do formulário seja obrigatório
 app.post("/", function(req, res){
     if(req.body.nome != null && req.body.email != null && req.body.sugest_usr != null){
         Sugest.create({
@@ -44,6 +48,7 @@ app.post("/", function(req, res){
     }
 });
 
+//Roda toda a aplicação na rede local usando a porta "8080"
 app.listen("8080", function(){
     console.log("Servidor rodando em http://localhost:8080");
 }); //Está sempre tem que ser a última linha do código de um projeto com express
--- public/sendform.js
+++ public/sendform.js
@@ -1,25 +1,26 @@
-var form = document.getElementById("formulario");
+var form = document.getElementById("formulario");//Variável que armazena a tag form do formulário.
 
-function submit_suggest_form(e) {
-    e.preventDefault();
-    fetch('/', {
+function submit_suggest_form(e) {//Função que envia o formulário de sugestão sem recarregar a página
+    e.preventDefault();//Cancela o evento de recarregamento da página
+    fetch('/', {//Reconhece a rota do formulário, o método "Post", e insere o JSON para trabalhar com os objetos
         method : "POST",
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
           },
-        body: JSON.stringify({
+        body: JSON.stringify({//Transforma os objetos listados em um JSON
             nome: document.getElementById("nome_campo").value, 
             email: document.getElementById("email_campo").value,
             sugest_usr: document.getElementById("sugestao_campo").value
         })
     })
+    //Limpa o formulário e da a confirmação do envio quando o formulário é enviado
     document.getElementById("nome_campo").value = '';
     document.getElementById("email_campo").value = '';
     document.getElementById("sugestao_campo").value = '';
     alert("Sugestão enviada com sucesso!");
 
-    return false;
+    return false;//Faz com que a página não reconheça o envio do formulário, ou seja, não redireciona o usuário
 }
 
-form.addEventListener('submit', submit_suggest_form);
+form.addEventListener('submit', submit_suggest_form);//executa a função quando um evento "submit" é lançado

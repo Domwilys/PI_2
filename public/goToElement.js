69c1aaad849a25163fe0353aaa76938ba7cf1f59
Willyan Paproski Bueno <willyan@pop-os.localdomain>
2022-10-27 15:23:08 -0300

    Redirecionamento de elementos dentro da pagina feito com sucesso

---
+++ public/goToElement.js
@@ -0,0 +1,14 @@
+function contatoRedirect(event) {
+    event.preventDefault()
+    window.scroll(0, 3000);
+}
+
+function topicoRedirect(event) {
+    event.preventDefault()
+    window.scroll(0, 500);
+}
+
+function sobreRedirect(event) {
+    event.preventDefault()
+    window.scroll(0, 1700)
+}

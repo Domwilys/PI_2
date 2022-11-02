0f94d5161a641c4e679fd0c71a26694ffa9356df
Willyan Paproski Bueno <willyan@pop-os.localdomain>
2022-11-02 19:32:01 -0300

    Adicao de rotas nas imagens dos topicos

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const handlebars = require('express-handlebars');//Insere o "handlebars" no cód
 const bodyParser = require('body-parser');//Insere o "bodyParser" no código
 const Sugest = require('./models/Sugest');//Armazena as tabelas do banco de dados criadas no arquivo "Sugest.js"
 const ip = require("ip");
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const dom = new JSDOM(`<!DOCTYPE html>`);
 
 app.use(express.static('public'));//Deixa a pasta "public" pública 
 app.use(express.static('imagens'));//Deixa a pasta de imagens pública

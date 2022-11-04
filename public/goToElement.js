c179eeb5b4a7d8af282cce6ec086c66b359a5162
Willyan Paproski Bueno <willyan@pop-os.localdomain>
2022-11-03 23:07:26 -0300

    links no rodape

--- public/goToElement.js
+++ public/goToElement.js
@@ -1,14 +1,19 @@
 function contatoRedirect(event) {
-    event.preventDefault()
+    event.preventDefault();
     window.scroll(0, 3000);
 }
 
 function topicoRedirect(event) {
-    event.preventDefault()
+    event.preventDefault();
     window.scroll(0, 500);
 }
 
 function sobreRedirect(event) {
-    event.preventDefault()
-    window.scroll(0, 1700)
+    event.preventDefault();
+    window.scroll(0, 1700);
+}
+
+function noticiasRedirect(event) {
+    event.preventDefault();
+    window.scroll(0, 1100);
 }
